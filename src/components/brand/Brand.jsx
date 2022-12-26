52f53160a59dd86c9b92a8a38f6375845231f04d
Chrisesbueno <alvarezchristopherve@gmail.com>
2022-12-26 12:44:24 -0400

    Proyecto GPT-3 Commit 3

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -3,15 +3,15 @@ import './brand.css'
 import {google, slack, atlassian, dropbox, shopify} from './imports'
 
 const Brand = () => {
-  const arrayBrand = [google, slack, atlassian, dropbox, shopify]
+  // const arrayBrand = [google, slack, atlassian, dropbox, shopify]
   
   return (
     <div className='brand'>
-      {arrayBrand.map((brand, index) => (
+      {/* {arrayBrand.map((brand, index) => (
         <div key={index}>
           <img src={brand}/>
         </div>
-      ))}
+      ))} */}
     </div>
   )
 }
