9e0b1c752077296da851b44f05659f39b7fbcc36
Chrisesbueno <alvarezchristopherve@gmail.com>
2022-12-26 12:40:28 -0400

    Proyecto GPT-3 Commit 3

--- src/components/brand/imports.js
+++ src/components/brand/imports.js
@@ -1,13 +1,5 @@
-import google from '../../assets/google.png'
-import slack from '../../assets/slack.png'
-import atlassian from '../../assets/atlassian.png'
-import dropbox from '../../assets/dropbox.png'
-import shopify from '../../assets/shopify.png'
-
-export {
-    google,
-    slack,
-    atlassian,
-    dropbox,
-    shopify
-}
+export { default as google } from '../../assets/google.png';
+export { default as slack } from '../../assets/slack.png';
+export { default as atlassian } from '../../assets/atlassian.png';
+export { default as dropbox } from '../../assets/dropbox.png';
+export { default as shopify } from '../../assets/shopify.png';
